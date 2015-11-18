fe77606a222a483008d70cdf9f515215dc2bdb9b
Robert Foy <robert.foy@gmail.com>
2015-11-18 16:07:26 +1000

    Added standard Tests button

--- v1super.user.js
+++ v1super.user.js
@@ -5,13 +5,15 @@
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js
 // @require       https://gist.githubusercontent.com/BrockA/2625891/raw/fd02ec05e3079cdd52cf5892a7ba27b67b6b6131/waitForKeyElements.js
 // @author        rf5860
-// @version       0.1
+// @version       0.2
 // @updateURL     https://github.com/rf5860/V1-Extra-Buttons/raw/master/v1-extra-buttons.user.js
 // @downloadURL   https://github.com/rf5860/V1-Extra-Buttons/raw/master/v1-extra-buttons.user.js
 // ==/UserScript==
 // Style
 var TASK_URL =  'https://www11.v1host.com/VentyxProd/rest-1.v1/Data/Task';
+var TEST_URL =  'https://www11.v1host.com/VentyxProd/rest-1.v1/Data/Test';
 var TASK_CREATE_RELATIVE_URL = "/VentyxProd/rest-1.v1/New/Task";
+var TEST_CREATE_RELATIVE_URL = "/VentyxProd/rest-1.v1/New/Test";
 var DEV_TASK_NAME = 'Development';
 var CODE_REVIEW_TASK_NAME = 'Code Review';
 var AUTOMATED_TESTING_TASK_NAME = 'Automated Testing';
@@ -26,30 +28,63 @@ var DOCUMENTATION_TASK_CATEGORY = '493515';
 
 var DATA_TYPE = 'application/xml';
 
-var STORY_TYPE = 'Story';
-var DEFECT_TYPE = 'Defect';
-
 var TASK_DATA = '      <Asset href="'+TASK_CREATE_RELATIVE_URL+'">'+
-        '       <Attribute name="Name" act="set">{Name}</Attribute>'+
-        '       <Relation name="Parent" act="set">'+
-        '       <Asset href="VentyxProd/rest-1.v1/Data/{Type}/{Id}" idref="{Parent}" />'+
-        '       </Relation>'+
-        '       <Relation name="Category" act="set">'+
-        '       <Asset href="VentyxProd/rest-1.v1/Data/TaskCategory/{CategoryId}" idref="TaskCategory:{CategoryId}"/>'+
-        '       </Relation>'+
-        '       </Asset>';
+    '       <Attribute name="Name" act="set">{Name}</Attribute>'+
+    '       <Relation name="Parent" act="set">'+
+    '       <Asset href="VentyxProd/rest-1.v1/Data/{Type}/{Id}" idref="{Parent}" />'+
+    '       </Relation>'+
+    '       <Relation name="Category" act="set">'+
+    '       <Asset href="VentyxProd/rest-1.v1/Data/TaskCategory/{CategoryId}" idref="TaskCategory:{CategoryId}"/>'+
+    '       </Relation>'+
+    '       </Asset>';
+
+
+var TEST_DATA = '      <Asset href="'+TEST_CREATE_RELATIVE_URL+'">'+
+    '       <Attribute name="Name" act="set">{Name}</Attribute>'+
+    '       <Relation name="Parent" act="set">'+
+    '       <Asset href="VentyxProd/rest-1.v1/Data/{Type}/{Id}" idref="{Parent}" />'+
+    '       </Relation>'+
+    '       <Relation name="Category" act="set">'+
+    '       </Relation>'+
+    '       </Asset>';
+
+function createTest(parent, name, category) {
+    var data = TEST_DATA.replace('{Name}', name)
+        .replace('{Parent}', parent)
+        .replace('{Type}', parent.split(':')[0])
+        .replace('{Id}', parent.split(':')[1]);
+    console.log('Request data: '+data);
+    $.ajax({url:TEST_URL, type:'POST',
+        data: data,
+        contentType:DATA_TYPE});
+}
 
 function createTask(parent, name, category) {
     var data = TASK_DATA.replace('{Name}', name)
-            .replace('{Parent}', parent)
-            .replace('{Type}', parent.split(':')[0])
-            .replace('{CategoryId}', category)
-            .replace('{CategoryId}', category)
-            .replace('{Id}', parent.split(':')[1]);
+        .replace('{Parent}', parent)
+        .replace('{Type}', parent.split(':')[0])
+        .replace('{CategoryId}', category)
+        .replace('{CategoryId}', category)
+        .replace('{Id}', parent.split(':')[1]);
     console.log('Request data: '+data);
     $.ajax({url:TASK_URL, type:'POST',
-            data: data,
-            contentType:DATA_TYPE});
+        data: data,
+        contentType:DATA_TYPE});
+}
+
+function createTests(parent) {
+    createTest(parent, DEV_TASK_NAME, DEVELOPMENT_TASK_CATEGORY);
+    createTest(parent, TESTING_TASK_NAME, TESTING_TASK_CATEGORY);
+    createTest(parent, AUTOMATED_TESTING_TASK_NAME, DEVELOPMENT_TASK_CATEGORY);
+    createTest(parent, CODE_REVIEW_TASK_NAME, DEVELOPMENT_TASK_CATEGORY);
+    window.location.reload();
+}
+
+function createStoryTests(parent) {
+    createTest(parent, DOCUMENTATION_TASK_NAME, DOCUMENTATION_TASK_CATEGORY);
+    createTest(parent, DOCUMENTATION_REVIEW_TASK_NAME, DOCUMENTATION_TASK_CATEGORY);
+    createTest(parent, TEST_CASE_REVIEW_TASK_NAME, TESTING_TASK_CATEGORY);
+    createTests(parent);
 }
 
 function createTasks(parent) {
@@ -67,20 +102,45 @@ function createStoryTasks(parent) {
     createTasks(parent);
 }
 
-var added = false;
+var addedTasks = false;
+var addedTests = false;
+function addTasksButton(d, oId) {
+    added = true;
+    var parent = $(d).parent().first();
+    var standardTasksButton = parent.clone()[0];
+    standardTasksButton.setAttribute('id', 'createStandardItemsButton');
+    standardTasksButton.children[0].removeAttribute('_handler');
+    standardTasksButton.children[0].textContent = 'Create Standard Tasks';
+    $(standardTasksButton).insertAfter(parent);
+    $(standardTasksButton).click(function () {
+        oId.startsWith('S') ? createStoryTasks(oId) : createTasks(oId);
+    });
+    $('<div style="width:0.2em" class="actions">').insertBefore($(standardTasksButton));
+}
+
+function addTestsButton(d, oId) {
+    added = true;
+    var parent = $(d).parent().first();
+    var standardTasksButton = parent.clone()[0];
+    standardTasksButton.setAttribute('id', 'createStandardItemsButton');
+    standardTasksButton.children[0].removeAttribute('_handler');
+    standardTasksButton.children[0].textContent = 'Create Standard Tests';
+    $(standardTasksButton).insertAfter(parent);
+    $(standardTasksButton).click(function () {
+        oId.startsWith('S') ? createStoryTests(oId) : createTests(oId);
+    });
+    $('<div style="width:0.2em" class="actions">').insertBefore($(standardTasksButton));
+}
+
 function addButons(d) {
     var oId = $('body>div[data-oid-token]').attr('data-oid-token');
     console.log('Id Token=['+oId+']');
-    if (!added) {
-        added = true;
-        var parent = $(d).parent().first();
-        var createElem = parent.clone()[0];
-        createElem.setAttribute('id', 'createStandardItemsButton');
-        createElem.children[0].removeAttribute('_handler');
-        createElem.children[0].textContent = 'Create Standard Tasks';
-        $(createElem).insertAfter(parent);
-        $(createElem).click(function () { oId.startsWith('S') ? createStoryTasks(oId) : createTasks(oId); });
-        $('<div style="width:0.2em" class="actions">').insertBefore($(createElem));
+    if (!addedTasks) {
+        addTasksButton(d, oId);
+        addedTasks = true;
+    } else if (!addedTests) {
+        addTestsButton(d, oId);
+        addedTests = true;
     }
 }
 
